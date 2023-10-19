d264dfff13ea0a497d505f70a57d272a2dfbbb08
tsakuru tazaki <hungrycoder619@gmail.com>
2023-10-19 15:00:29 +0545

    deleted node modules

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
-const createServer = require('http').createServer;
-const Server = requir('socket.io').server;
+const { createServer } = require("http");
+const { Server } = require("socket.io");
 
-const httpServer = createServer();
-const io = new Server(httpServer, {
-  cors: {
-    origin: "*"
+
+const httpServer = createServer((req, res) => {
+  if (req.url == "/") {
+    res.end("Running...")
   }
-});
-
+})
+const io = new Server(httpServer, { cors: '*' });
 
 io.on('connection', (socket) => {
   console.log(socket.id)
+
 })
+httpServer.listen(8080, () => {
+  console.log('Server listining on port 8080')
+});
