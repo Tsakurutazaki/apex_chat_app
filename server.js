af0d1790bf42ec2fdb03f3afec507dd72dab5efa
tsakuru tazaki <hungrycoder619@gmail.com>
2023-11-08 13:59:02 +0545

    changed code

--- server.js
+++ server.js
@@ -1,23 +1,51 @@
-const io = require('socket.io')(3000) // create server
+const createServer = require('http').createServer;
+const Server = require('socket.io').Server;
 
-const users = {}
+// import { createServer } from 'http'
+// import { server } from 'socket.io' 
 
+const httpServer = createServer()
+
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*"
+  }
+});
 
 io.on('connection', socket => {
+  console.log(`User ${socket.id} connected`)
 
-  socket.on('new-user', name => {
-    users[socket.id] = name
-    socket.broadcast.emit('user-connected', name)
+  socket.on('message', data => {
+    console.log(data)
+    io.emit('message', `${socket.id.substring(0,5)} : ${data}`)
   })
+});
 
-  //socket.emit('chat-message', 'hello world')
-  socket.on('send-chat', data => {
+httpServer.listen(8080, () => console.log('it is listening on port 8080'))
 
-    socket.broadcast.emit('message-received', {data: data, user: users[socket.id]})
-  })
 
-  socket.on('disconnect', ()=> {
-    socket.broadcast.emit('user-disconnected', users[socket.id])
-    delete users[socket.id]
-  })
-})
+
+
+// const io = require('socket.io')(3000) // create server
+
+// const users = {}
+
+
+// io.on('connection', socket => {
+
+//   socket.on('new-user', name => {
+//     users[socket.id] = name
+//     socket.broadcast.emit('user-connected', name)
+//   })
+
+//   //socket.emit('chat-message', 'hello world')
+//   socket.on('send-chat', data => {
+
+//     socket.broadcast.emit('message-received', {data: data, user: users[socket.id]})
+//   })
+
+//   socket.on('disconnect', ()=> {
+//     socket.broadcast.emit('user-disconnected', users[socket.id])
+//     delete users[socket.id]
+//   })
+// })
